ef9934bba0bba437e0c2547c9b13d0bc95c68078
Flavio Moraes <flaviomrs2002@gmail.com>
2023-10-12 03:53:09 -0300

    Criação do template html base

---
+++ templates/base.js
@@ -0,0 +1,9 @@
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
