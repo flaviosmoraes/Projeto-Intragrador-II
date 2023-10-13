fc664cd76b2b27a9b6010d3ebf136fea181e4fec
Flavio Moraes <flaviomrs2002@gmail.com>
2023-10-12 23:27:02 -0300

    apps mapa e gerenciamento criados e configurados

--- compostcollector/urls.py
+++ compostcollector/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include("home.urls")),
+    path('', include('home.urls')),
+    path('mapa', include('mapa.urls')),
+    path('gerenciamento', include('gerenciamento.urls'))
 ]
---
+++ gerenciamento/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def gerenciamento(request):
+    return render(request, 'gerenciamento.html')
